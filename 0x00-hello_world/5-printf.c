c92f3816b63d70405c86cf4460be44890b23f91a
lol-DDev <sams.eagric@gmail.com>
2022-06-02 21:47:20 -0700

    adding commits

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
  *
  *                 a piece of art," followed by a new line.
  *
- *   
+ *
  *
  *  * Return: Always 0 (Success)
  *
@@ -17,9 +17,8 @@
 int main(void)
 
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
+	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
